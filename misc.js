3ce4be0111d60401f632519ae91bbc65b0134825
Reikimann <cbirkmand@gmail.com>
2023-10-24 15:15:13 +0200

    Changes now apply only if the element exists. This prevents errors.

--- misc.js
+++ misc.js
@@ -2,11 +2,11 @@ document.getElementById("masterContent").style.width = "100vw";
 document.getElementsByClassName("ls-master-container2")[0].style.width = "100vw";
 
 // Dashbord
-const layoutElms = document.getElementsByClassName("ls-std-island-layout-ltr")
+const layoutElms = document.getElementsByClassName("ls-std-island-layout-ltr");
 if (layoutElms.length !== 0) {
   for (let i = 0; i < layoutElms.length; i++) {
     layoutElms[i].style.width = "calc(100vw - 15px)";
-    
+
     const children = layoutElms[i].children;
     for (let x = 0; x < children.length; x++) {
       children[x].style.flex = "1";
@@ -16,23 +16,23 @@ if (layoutElms.length !== 0) {
   layoutElms[0].parentElement.children[2].style.width = "calc(100vw - 25px)";
 }
 
-// Skema changes
-document.getElementById("s_m_Content_Content_SkemaNyMedNavigation_skema_skematabel").style.width = "calc(100vw - 25px)";
-document.getElementById("s_m_Content_Content_SkemaNyMedNavigation_skema_skematabel").style.maxWidth = "none";
+// Schedule changes
+const scheduleTable = document.getElementById("s_m_Content_Content_SkemaNyMedNavigation_skema_skematabel");
+if (scheduleTable) {
+  scheduleTable.style.width = "calc(100vw - 25px)";
+  scheduleTable.style.maxWidth = "none";
+}
 
 const skemaContainers = document.getElementsByClassName("s2skemabrikcontainer");
-console.log(skemaContainers)
 if (skemaContainers.length !== 0) {
   // Set columns width
   for (let i = 0; i < skemaContainers.length; i++) {
     console.log(skemaContainers[i], skemaContainers[i].parentElement.offsetWidth)
-    // skemaContainers[i].style.width = skemaContainers[i].parentElement.offsetWidth + "px"
-    const width = skemaContainers[i].offsetWidth;
     skemaContainers[i].style.width = `15.9vw`;
   }
 
   const classes = document.getElementsByClassName("s2skemabrik");
-  const mapping = {
+  const mapping = { // Schedule entry width vs amount of elements that day
     "13.82em": 1,
     "9.09em": 1,
     "6.73em": 2,
